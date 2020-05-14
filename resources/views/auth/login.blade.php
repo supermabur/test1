07e685b0a8df3e27c058eee81c40310c71c912b4
supermabur <supermabur@gmail.com>
2020-05-14 15:56:10 +0700

    strole

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,6 +11,7 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
+                        {{$errors}}
                         <div class="form-group row">
                             <label for="login" class="col-sm-4 col-form-label text-md-right">
                                 {{ __('Username or Email') }}
