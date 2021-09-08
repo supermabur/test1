d13149164775b5af0ab0d356d2eef557974d2660
supermabur <supermabur@gmail.com>
2021-09-08 14:00:36 +0700

    asasd

--- app/Http/Controllers/rptpesanestkirimmemoController.php
+++ app/Http/Controllers/rptpesanestkirimmemoController.php
@@ -34,8 +34,9 @@ class rptpesanestkirimmemoController extends Controller
         $platform = $agent->platform();
         $device = $agent->device();
         $version = $agent->version($platform);
+        $ip = $request->ip() . '-' . $this->getClientIPaddress($request);;
 
-        $aksesby = $browser . ' ' . $platform . ' ' . $version . ' ' . $device;
+        $aksesby = $browser . ' ' . $platform . ' ' . $version . ' ' . $device . ' ' . $ip;
 
         DB::table('log_akses')->insert([
             ['aksesmodul' => 'rptpesanestimasikirimmemo', 'aksesby' => $aksesby]
@@ -44,5 +45,29 @@ class rptpesanestkirimmemoController extends Controller
 
         return view('rptpesanestkirimmemo',compact('mendekatiestkirim', 'gaadamemo', 'lastupdate'));
     }
+
+
+    public function getClientIPaddress(Request $request) {
+
+        if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+            $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+            $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+        }
+        $client  = @$_SERVER['HTTP_CLIENT_IP'];
+        $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+        $remote  = $_SERVER['REMOTE_ADDR'];
+    
+        if(filter_var($client, FILTER_VALIDATE_IP)){
+            $clientIp = $client;
+        }
+        elseif(filter_var($forward, FILTER_VALIDATE_IP)){
+            $clientIp = $forward;
+        }
+        else{
+            $clientIp = $remote;
+        }
+    
+        return $clientIp;
+     }
 
 }
